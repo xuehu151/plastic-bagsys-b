1ce51e23eb8893f7846d8263f4324e0a5b12effc
xuehu151 <837769474@qq.com>
2020-02-08 12:08:22 +0800

    api url and service url

--- src/app/providers/service.config.ts
+++ src/app/providers/service.config.ts
@@ -3,11 +3,12 @@ import { environment } from '../../environments/environment';
 export class ServiceConfig {
     // 获取首页数据
     public static APIBASE = environment.apiBase;
-    public static LOGIN = '/public/loginByPassword'; // 登录
-    public static GETUSERINFO = '/sys/user/getUserInfo';//获取用户信息
-    public static BINDCALLBACK = '/public/bindCallback/'; //第三方绑定
-    public static RENDER = '/public/render/';
-    public static AGENTFUND = '/biz/agent/agentFund';
+    public static LOGIN = 'public/loginByPassword'; // 登录
+    public static GETUSERINFO = 'sys/user/getUserInfo';//获取用户信息
+    public static BINDCALLBACK = 'public/bindCallback/'; //第三方绑定
+    public static RENDER = 'public/render/';
+    public static AGENTFUND = 'biz/agent/agentFund';
+    public static WITHDRAW = 'biz/withdraw/add';//提现
 
 
 }
